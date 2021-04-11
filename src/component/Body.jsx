0503c51b5d59653f7f4d5c7852a52231cdd353c9
Shashi <shashitiwari1997@gmail.com>
2021-04-11 09:50:07 +0530

    radio App

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./cssFile/body.css";
 import Card from "./Card";
-import { item } from "./fetch.js";
+// import { item } from "./fetch.js";
 function Body() {
   //Fetching the data. from database ...
-  
+
   return (
     <div className="bodyBar">
       <Card />
