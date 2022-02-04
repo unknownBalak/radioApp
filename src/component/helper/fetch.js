05ff52a3aae620e9655f53255e49e3c7954f55a2
shashi <shashitiwari1997@gmail.com>
2022-02-04 15:52:39 +0530

    added

--- src/component/helper/fetch.js
+++ src/component/helper/fetch.js
@@ -1,21 +1,29 @@
 import axios from "axios";
-let url = "https://fr1.api.radio-browser.info/json/stations";
-function makeGetRequest(url) {
-  return new Promise(function (resolve, reject) {
-    let res = axios.get(url);
-    try {
-      res.then(
-        (res) => {
-          resolve(res.data);
-        },
-        (error) => {
-          reject(error);
-        }
-      );
-    } catch {
-      console.log("error");
-    }
-  });
+let url = "https://fr1.api.radio-browser.info/json/stations21";
+async function makeGetRequest(url) {
+  let res = "";
+  try {
+    res = await axios.get(url);
+    console.log("THIs ires ", res);
+    return res.data;
+  } catch (err) {
+    console.log("This iserr", err);
+  }
+  // return new Promise(function (resolve, reject) {
+  //   let res = axios.get(url);
+  //   try {
+  //     res.then(
+  //       (res) => {
+  //         resolve(res.data);
+  //       },
+  //       (error) => {
+  //         reject(error);
+  //       }
+  //     );
+  //   } catch {
+  //     console.log("error");
+  //   }
+  // });
 }
 async function main() {
   let result = await makeGetRequest(url);
